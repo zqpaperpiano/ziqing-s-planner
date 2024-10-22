98fbcec9a37116230d0bf6768627133e3ad10418
zqpaperpiano <ziqing.ong.2021@socsc.smu.edu.sg>
2024-10-22 16:15:31 +0800

    navbar contents + hover

--- src/components/AdventureLog/AdventureLog.jsx
+++ src/components/AdventureLog/AdventureLog.jsx
@@ -1,19 +1,40 @@
 import React from "react";
-import './AdventureLog.css'
+import './AdventureLog.css';
+import OverviewIcon from '../../images/overview-icon.png';
+import QuestIcon from '../../images/quest-icon.png';
+import AdventureIcon from '../../images/adventure-icon.png';
+import ShopIcon from '../../images/shop.png';
 
 const AdventureLog = () => {
     return(
         <div className="navbar-container">
             <div className="item">
+                <div className="navbar-icon">
+                    <img src={OverviewIcon} className="icon"    />
+                </div>
+                
                 <a>Overview</a>
             </div>
+                
             <div className="item">
+                <div className="navbar-icon">
+                    <img src={QuestIcon} className="icon"/>
+                </div> 
+                
                 <a>Quest Board</a>
             </div>
-            <div className="item1">
+            <div className="item">
+                <div className="navbar-icon">
+                    <img src={AdventureIcon} className="icon"/>
+                </div>
+                
                 <a>Adventure!</a>
             </div>
-            <div className="item1">
+            <div className="item">
+                <div className="navbar-icon">
+                    <img src={ShopIcon} className="icon"/>
+                </div>
+                
                 <a>Store</a>
             </div>
         </div>
