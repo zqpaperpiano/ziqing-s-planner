98fc18b02aaf1fa04edcb763dfe1406dea361515
zqpaperpiano <ziqing.ong.2021@socsc.smu.edu.sg>
2025-03-28 12:21:50 +0800

    linked dungeon creation page

--- src/components/QuestBoard/DungeonDetail/DungeonDetailInput.jsx
+++ src/components/QuestBoard/DungeonDetail/DungeonDetailInput.jsx
@@ -95,7 +95,7 @@ const DungeonDetailInput = ({handleExitAddDungeon, handleIncreaseDungeons }) => 
                 <div className="relative bg-deepPink flex w-full p-2" style={{height: '12%', fontFamily: 'source-code-pro'}}>
                     <p className="text-3xl font-bold">New Dungeon</p>
                     <div className="absolute right-0 h-full top-0 flex items-center w-12  hover:cursor-pointer hover:bg-opacity-30 hover:bg-white">
-                        <XIcon onClick={handleExitAddDungeon} size={50} color="" strokeWidth={0.25} radius={1} />
+                        <XIcon onClick={handleExitAddDungeon}  size={50} color="" strokeWidth={0.25} radius={1} />
                     </div>
                     <div className="absolute right-12 w-12 top-0 h-full flex items-center hover:cursor-pointer hover:bg-opacity-30 hover:bg-white">
                         <CheckIcon onClick={handlePostQuest} size={50} color="" strokeWidth={0.25} radius={1} />
@@ -105,12 +105,12 @@ const DungeonDetailInput = ({handleExitAddDungeon, handleIncreaseDungeons }) => 
                 <div className="flex w-full py-2 px-4 flex-col overflow-y-auto " style={{height: '88%', fontFamily: 'source-code-pro'}}>
                         <div className="h-fit flex flex-col gap-2 w-full">
                             <p className="text-xl">Dungeon Name: </p>
-                            <input type="text" className="w-full border border-black border-1" />
+                            <input onChange={(e) => {handleDungeonDetailsChange(e, 'dungeonName')}} type="text" className="w-full border border-black border-1" />
                         </div>
 
                         <div className="h-fit flex flex-col gap-2 w-full">
                             <p className="text-xl">Dungeon Description: </p>
-                            <textarea className="w-full border border-black border-1 resize-none" rows={7} />
+                            <textarea onChange={(e) => {handleDungeonDetailsChange(e, 'dungeonDescription')}}  className="w-full border border-black border-1 resize-none" rows={7} />
                         </div>
 
                         <div className="h-fit flex flex-col gap-2 mt-2">
